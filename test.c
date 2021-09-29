9d7afbe7aff939e4e708d10e58321cbe4aa6a53d
Luca Burlizzi <luca.burlizzi@airbus.com>
2021-09-29 10:38:46 +0200

    some test changes

--- test.c
+++ test.c
@@ -2,11 +2,13 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#define S(x) x,sizeof(x)
 
 #define PAGE_SIZE     4096
 
 int main (int argc, char **argv)
 {
+    printf (S("la dimensione di  questa stringa e'%d\n"));
 
     int configfd;
     char *address = NULL;
@@ -18,23 +20,24 @@ int main (int argc, char **argv)
         return -1;
     }
 
-    address = mmap (NULL, PAGE_SIZE*200, PROT_READ | PROT_WRITE, MAP_SHARED, configfd, 0);
+    address = mmap (NULL, PAGE_SIZE*10, PROT_READ | PROT_WRITE, MAP_SHARED, configfd, 0);
     if (address == MAP_FAILED)
     {
         perror ("mmap operation failed");
         return -1;
     }
 
+
     //memcpy (address + 11, "*user*", 6);
     printf ("Initial message: %s\n", address);
     //sleep(1);
-    memcpy (address + 11, "*user*", 6);
-    sleep(1);
-    memcpy (address + PAGE_SIZE-10, "Hello from *user* this is file:", 126);
+    memcpy (address , "user", 6);
+    //sleep(1);
+    memcpy (address + PAGE_SIZE-10, S("Hello from *user* this is file:"));
     //memcpy (address + 11, "*mio**", 6);
-    sleep(1);
+    //sleep(1);
     printf ("Changed message: %s\n", address);
-    sleep(1);
+    //sleep(1);
     printf ("Changed message: %s\n", address+PAGE_SIZE-10);
     //sleep(1);
     close (configfd);
